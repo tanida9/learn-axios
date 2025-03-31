e63e38df49c85e89cd80dea97773baa57e0cf5f9
tanida <tanida.manee@gmail.com>
2025-03-31 21:12:16 +0700

    feat(file): Created MyAxiosAsyncAwait.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,46 +1,53 @@
-import { AxiosAsyncAwait } from "./components/AxiosAsyncAwait";
-import { AxiosThenCatch } from "./components/AxiosThenCatch";
-import { FetchPromise } from "./components/FetchPromise";
-import { APIExplorer } from "./components/APIExplorer";
-import { FetchAsyncAwait } from "./components/FetchAsyncAwait";
+//import { AxiosAsyncAwait } from "./components/AxiosAsyncAwait";
+//import { AxiosThenCatch } from "./components/AxiosThenCatch";
+//import { FetchPromise } from "./components/FetchPromise";
+//import { APIExplorer } from "./components/APIExplorer";
+//import { FetchAsyncAwait } from "./components/FetchAsyncAwait";
 // import { APIExplorer2 } from "./components/APIExplorer2";
+
+import { MyAxiosAsyncAwait } from "./myComponent/MyAxiosAsyncAwait";
+
 
 export default function App() {
   return (
-    <div className="min-h-screen flex justify-center bg-blue-950">
+    <div className="min-h-screen flex justify-center bg-orange-200">
       <div className="p-6 gap-y-6 flex flex-col justify-start w-[80%] lg:w-[70%]">
-        <h1 className="w-full p-6 bg-amber-100 font-extrabold">
+        {/*<h1 className="w-full p-6 bg-amber-100 font-extrabold rounded-xl">
           First Meet with Axios
-        </h1>
-        <section className="w-full p-5 bg-amber-100 flex">
+        </h1> */}
+        {/* <section className="w-full p-5 bg-amber-100 flex rounded-xl">
           <ul className="list-inside list-disc flex-1">
             <span className="font-semibold">Examples:</span>
-            <li>Axios with Async/Await</li>
+            <li>Axios with Async/Await ✅</li>
             <li>Axios with Then/Catch</li>
             <li>Fetch Async/Await</li>
             <li>Fetch Then/Catch</li>
             <li>API Explorer</li>
           </ul>
-        </section>
+        </section> */}
 
-        <section className="w-full p-5 bg-amber-100 flex">
+        {/*<section className="w-full p-5 bg-amber-100 flex">
           <AxiosAsyncAwait />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
+        </section > */}
+        {/*<section className="w-full p-5 bg-amber-100 flex">
           <AxiosThenCatch />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
+        </section> */}
+        {/*<section className="w-full p-5 bg-amber-100 flex">
           <FetchAsyncAwait />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
+        </section> */}
+        {/*<section className="w-full p-5 bg-amber-100 flex">
           <FetchPromise />
-        </section>
-        <section className="w-full p-5 bg-amber-100 flex">
+        </section> */}
+        {/*<section className="w-full p-5 bg-amber-100 flex">
           <APIExplorer />
-        </section>
+        </section> */}
         {/* <section className="w-full p-5 bg-amber-100 flex">
           <APIExplorer2 />
         </section> */}
+        <section className="w-full p-5 bg-amber-100 flex rounded-xl">
+          <MyAxiosAsyncAwait />
+        </section>
+
       </div>
     </div>
   );
---
+++ src/myComponent/MyAxiosAsyncAwait.jsx
@@ -0,0 +1,48 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
+
+export function MyAxiosAsyncAwait() {
+  const [users, setUsers] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const fetchUsers = async () => {
+      try {
+        const res = await axios.get(
+          "https://api.thecatapi.com/v1/images/search?limit=10"
+        );
+        setUsers(res.data);
+      } catch (err) {
+        setError("Failed to fetch users");
+        console.error(err);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchUsers();
+  }, []);
+
+  return (
+    <div className="min-h-screen w-full  bg-blue-100 p-6 rounded-xl">
+      <h1 className="text-2xl font-bold mb-4 text-center">Random Cutie List😻</h1>
+
+      {loading && <p className="text-center">Loading...</p>}
+      {error && <p className="text-center text-red-500">{error}</p>}
+
+      <ul className="space-y-4 max-w-md mx-auto">
+        {users.map((user) => (
+          <li
+            key={user.id}
+            className=" bg-white shadow-md rounded-xl mb-6 p-4 hover:bg-gray-50 transition"
+          >
+            <img src={user.url} alt={user.id} />
+            <p className="text-gray-600">Width : {user.width}</p>
+            <p className="text-sm text-gray-400">Heigth : {user.height}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+}
